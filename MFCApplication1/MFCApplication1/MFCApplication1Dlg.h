dfb2c31b07dfbf9721e6eaa52c5daa8144b6459c
makky <75469056+makky-star@users.noreply.github.com>
2021-07-21 20:56:06 +0900

    initial commit

---
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,47 @@
+﻿
+// MFCApplication1Dlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CMFCApplication1Dlg ダイアログ
+class CMFCApplication1Dlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CMFCApplication1Dlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTcnSelchangeTab1(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnEnChangeEdit1();
+	afx_msg void OnLbnSelchangeList1();
+	afx_msg void OnEnChangeEdit2();
+	afx_msg void OnBnClickedRadio2();
+	afx_msg void OnEnChangeEdit3();
+	afx_msg void OnLbnSelchangeList2();
+	afx_msg void OnIpnFieldchangedIpaddress1(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnCbnSelchangeCombo3();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnTvnSelchangedMfcshelltree1(NMHDR* pNMHDR, LRESULT* pResult);
+};
---
+++ MFCApplication1/MFCApplication1/resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// MFCApplication1.rc で使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
+#define IDC_BUTTON2                     1007
+#define IDC_EDIT3                       1008
+#define IDC_SCROLLBAR2                  1009
+#define IDC_RADIO1                      1011
+#define IDC_RADIO2                      1012
+#define IDC_COMBO3                      1015
+#define IDC_DATETIMEPICKER1             1018
+#define IDC_TAB1                        1020
+#define IDC_SCROLLBAR1                  1033
+#define IDC_PROGRESS2                   1034
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1035
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
